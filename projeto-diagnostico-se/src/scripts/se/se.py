061930c422eecc7ff7efbe4a4bc64ebc7d9ced8f
ramones0202 <ramoni.reus.barros.negreiros@ccc.ufcg.edu.br>
2026-03-01 11:42:28 -0300

    feat: deleta artigos, constrói parte do KB

---
+++ projeto-diagnostico-se/src/scripts/se/se.py
@@ -0,0 +1,6 @@
+# COVID-19
+insuficiencia_cardiaca = True # Cardiovascular disease and COVID-19
+diarreia = True # Gastrointestinal symptoms associated with COVID-19: impact on the gut microbiome
+opacidade_vidro_fosco = True # Information about the new coronavirus disease (COVID-19)
+
+# Pneumonia Viral
