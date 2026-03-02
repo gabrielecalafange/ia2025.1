d731f6ea62a999b6e45e1d5f5295fbb3d65ecef8
ramones0202 <ramoni.reus.barros.negreiros@ccc.ufcg.edu.br>
2026-03-02 19:44:23 -0300

    feat: add limiares dos critérios p/ Covid-19

--- projeto-diagnostico-se/src/scripts/se/se.py
+++ projeto-diagnostico-se/src/scripts/se/se.py
@@ -1,6 +1,5 @@
 # COVID-19
-insuficiencia_cardiaca = True # Cardiovascular disease and COVID-19
-diarreia = True # Gastrointestinal symptoms associated with COVID-19: impact on the gut microbiome
-opacidade_vidro_fosco = True # Information about the new coronavirus disease (COVID-19)
+MCHC_ALTO = 0,334482 # 334,482 g/L
+LDH_ALTO = 0.0075 # 7,5 µkat/L
+PROTEINA_C_ALTA = 0.0001 # 10 mg/dL
 
-# Pneumonia Viral
