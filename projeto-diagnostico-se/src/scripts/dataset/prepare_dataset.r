d2c5a0275cc93497bbd949c9b3584ea891948a79
ramones0202 <ramoni.reus.barros.negreiros@ccc.ufcg.edu.br>
2026-03-02 09:48:45 -0300

    feat: add golden dataset e dataset filtrado

--- projeto-diagnostico-se/src/scripts/dataset/prepare_dataset.r
+++ projeto-diagnostico-se/src/scripts/dataset/prepare_dataset.r
@@ -1,11 +1,11 @@
 library(dplyr)
 library(tidyr)
 
-caminho_arquivo <- "/home/ramoni/Desktop/ia/ia-2025.2/ia2025.1/projeto-diagnostico-se/data/dataset_completo.csv"
+caminho_arquivo <- "/home/dell/ia-2026.1/ia2025.1/projeto-diagnostico-se/data/dataset_completo.csv"
 
 df <- read.csv(caminho_arquivo, 
                stringsAsFactors = FALSE, 
-               check.names = FALSE, 
+               check.names = FALSE,
                na.strings = c("", "NA", "NULL"))
 
 colunas_exames <- c(
@@ -22,11 +22,8 @@ colunas_exames <- c(
 
 df_final <- df %>%
   select(any_of(c("Patient ID", "SARS-Cov-2 exam result", "Influenza A, rapid test", "Influenza B, rapid test", colunas_exames))) %>%
-  drop_na(all_of(colunas_exames)) %>%
   slice(1:1000)
 
-cat("Total de registros com dados completos encontrados:", nrow(df_final), "\n")
+cat("Processamento concluído. Registros capturados:", nrow(df_final), "\n")
 
-View(df_final)
-
-write.csv(df_final, "/home/ramoni/Desktop/ia/ia-2025.2/ia2025.1/projeto-diagnostico-se/data/dataset_filtrado.csv")
+write.csv(df_final, "/home/dell/ia-2026.1/ia2025.1/projeto-diagnostico-se/data/dataset_filtrado.csv", row.names = FALSE)
